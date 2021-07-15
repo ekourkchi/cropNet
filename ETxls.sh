089f59475ba24fcb96271da7e57056230e7c983c
root <ekourkchi@gmail.com>
2021-07-15 06:17:57 -1000

    adding comments

--- ETxls.sh
+++ ETxls.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/bash
 
+# This script is run by the server everyday at 1:00 AM
+
 echo begin `date`
 
+# Setting up the python essentials
 . "/home/share/anaconda3/etc/profile.d/conda.sh"
 export PATH="/home/share/anaconda3/bin:$PATH"
 
+# Changing to the work directory
 cd /home/weather/GetData
 
 # Downloading new data and updating the database
@@ -16,12 +20,17 @@ python ET2XLS_HOBO.py
 # Adding Campbell stations to the Excel file
 python ET2XLS_Campbell.py
 
-# transerind data to the http zone
+# Transering data to the http zone
 cp HOBO_CAMPBELL_ET0_Rain.xlsx /home/weather/public_html/.
 
-# managing the ownership of the file
+# Managing the ownership of the file
 chown weather:weather /home/weather/public_html/HOBO_CAMPBELL_ET0_Rain.xlsx
 
+# Forecasting ETo and Rainfall for a list of stations
+# usage$ python forecaste_ETrain.py <seril_type> <serial_no>
+# serial_type: the data type of the serial number, either "integer" or "string"
+# serial_no: the serial number or the assigned code of the desired station
+# Note: the addressed station should have vlaid data input in the database
 
 python forecaste_ETrain.py integer 10906989
 python forecaste_ETrain.py integer 20173022

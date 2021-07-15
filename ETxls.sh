14fa926685b3fda4b36a210d24c52ec59f9ffab7
root <ekourkchi@gmail.com>
2021-07-15 06:23:32 -1000

    adding extra comments

--- ETxls.sh
+++ ETxls.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 
 # This script is run by the server everyday at 1:00 AM
+# This script has 2 important tasks:
+#    1. Downloading data from HOBO or CAMPBELL platforms and updating the backend database
+#    2. Making forecasts using a machine learning method
 
 echo begin `date`
 
@@ -26,11 +29,13 @@ cp HOBO_CAMPBELL_ET0_Rain.xlsx /home/weather/public_html/.
 # Managing the ownership of the file
 chown weather:weather /home/weather/public_html/HOBO_CAMPBELL_ET0_Rain.xlsx
 
+################################################################################
 # Forecasting ETo and Rainfall for a list of stations
 # usage$ python forecaste_ETrain.py <seril_type> <serial_no>
 # serial_type: the data type of the serial number, either "integer" or "string"
 # serial_no: the serial number or the assigned code of the desired station
 # Note: the addressed station should have vlaid data input in the database
+################################################################################
 
 python forecaste_ETrain.py integer 10906989
 python forecaste_ETrain.py integer 20173022
@@ -45,7 +50,6 @@ python forecaste_ETrain.py integer 20121045
 python forecaste_ETrain.py string  PH1
 python forecaste_ETrain.py string  PH2
 
-
-
+# record the time/date at the of the daily log file
 echo end `date`
 
